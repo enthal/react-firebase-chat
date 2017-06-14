ebdae22ccbfccb39882a7da418fa76227f45739b
Tim James <tjames@tjames.com>
2017-06-14 16:03:25 -0700

    classnames; confused pass at room selection (local/view state!)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase';
-import { connect } from 'react-firebase'
+import { connect } from 'react-firebase';
+import classNames from 'classnames';
 import * as _ from 'lodash';
 import './index.css';
 
@@ -17,17 +18,28 @@ firebase.initializeApp({
   messagingSenderId: "251175505954"
 });
 
+const RUN = () => ReactDOM.render(
+  <Chat />,
+  document.getElementById('root')
+);
+
+const viewState = {};
+const selectRoom = roomId => {
+  viewState.selectedRoomId = roomId;
+  // viewState.liveRooms.forceUpdate();
+  console.log(viewState);
+}
 
 const Chat = () => {
+  viewState.liveRooms = <LiveRooms passthru="passed!"/>;
   return (
     <div>
       <h1>Chat</h1>
       <LiveUsers />
-      <LiveRooms />
+      {viewState.liveRooms}
     </div>
   );
 }
-
 
 const LiveUsers = connect((props, ref) => ({
     users: 'users',
@@ -39,23 +51,58 @@ const LiveUsers = connect((props, ref) => ({
 );
 
 
-const LiveRooms = connect((props, ref) => ({
+const LiveRooms = connect(
+  (props, ref) => ({
     rooms: 'rooms',
     pushRoom: (what) => ref('rooms').push({what}),
-  })
+  }),
+  (ownProps, firebaseProps) => {
+    console.log('LiveRooms mergeProps:', ownProps, firebaseProps);
+    return Object.assign({the:'thing'}, ownProps, firebaseProps);
+  },
 )(
-  ({rooms, pushRoom}) =>
-    NamedThings("Rooms", rooms, pushRoom, Post)
+  ({rooms, pushRoom, passthru}) =>
+    NamedThings("Rooms", rooms, pushRoom, Room, passthru)
 );
 
+const Room = ({what}, id) => (
+  <div
+    className={classNames({selected: id === viewState.selectedRoomId})}
+    onClick={()=> selectRoom(id)}
+    >
+    {what}
+  </div>
+);
 
-const NamedThings = (title, thingsById, make, renderThing) => (
+/*
+class Room extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(e) {
+    this.setState(state => { selected:!state.selected });
+  }
+  render() {
+    <div
+      className={classNames({'selected':this.state.selected})}
+      onClick={this.onClick}>
+    </div>
+  }
+}
+// */
+
+
+const NamedThings = (title, thingsById, make, renderThing, the) => (
   <div className='named-things'>
     <h1>{title}</h1>
     <ul>
     {_.map(thingsById, (thing, id) => (
       <li key={id}>
-        {renderThing(thing)}
+        {renderThing(thing, id)}
       </li>
     ))}
     </ul>
@@ -63,6 +110,7 @@ const NamedThings = (title, thingsById, make, renderThing) => (
       buttonTitle="Make"
       onSubmit={what => make(what)}
       />
+    {the}
   </div>
 );
 
@@ -108,13 +156,7 @@ const Post = ({what}) => (
   </div>
 );
 
-
-ReactDOM.render(
-  <Chat />,
-  document.getElementById('root')
-);
-
-
+RUN();
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
